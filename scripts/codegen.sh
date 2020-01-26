531111b6f0c907aea7f1a8e200af5e7af4fae5b5
François Gannaz <francois.gannaz@silecs.info>
2020-01-26 16:26:59 +0100

    Python code style with black

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -2,5 +2,5 @@
 if [ -f pkg/vips/operators.go ]; then
   rm pkg/vips/operators.go
 fi
-python scripts/gen-operators.py >> pkg/vips/operators.go
+python2 scripts/gen-operators.py >> pkg/vips/operators.go
 gofmt -s -w pkg/vips/operators.go
