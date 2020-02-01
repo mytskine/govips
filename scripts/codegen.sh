a3afd3fbd26b92a947a4ed717ac52be3e9664c92
François Gannaz <francois.gannaz@silecs.info>
2020-02-01 17:11:27 +0100

    Document code gen and its requirements

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [ -f pkg/vips/operators.go ]; then
-  rm pkg/vips/operators.go
-fi
-python2 scripts/gen-operators.py >> pkg/vips/operators.go
+
+# Generates Go bindings for all the Vips operators
+# that have bindings for their argument types.
+
+echo "Requires: Python2 GObject Inspection, libvips 8+ devel"
+echo "     e.g. apt install python-gi libvips-dev"
+
+echo Generating the operators...
+python2 scripts/gen-operators.py > pkg/vips/operators.go
+echo Formating the Go source code...
 gofmt -s -w pkg/vips/operators.go
